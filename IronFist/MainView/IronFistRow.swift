0d444e9a277d35bd1d485473acf23a2a5eac7612
Marc Respass <marc@iliosinc.com>
2021-07-01 14:42:12 -0400

    Don't highlight the rows in the table view

--- IronFist/MainView/IronFistRow.swift
+++ IronFist/MainView/IronFistRow.swift
@@ -8,25 +8,17 @@
 import SwiftUI
 
 struct IronFistRow: View {
-    @EnvironmentObject var controller: IronFistController
-
     let ironFist: IronFist
 
     var body: some View {
-        let highlight = ironFist == self.controller.selectedIronFist
         VStack(alignment: .leading) {
             HStack {
-                if highlight {
-                    Image(systemName: "chevron.forward.circle.fill")
-                }
                 Text("\(ironFist.id).")
                 Text(ironFist.title)
             }
             Text(ironFist.instruction)
                 .lineLimit(1)
-                .font(highlight ? .body : .caption)
         }
-        .listRowBackground(highlight ? Color.green: Color.clear)
     }
 }
 
