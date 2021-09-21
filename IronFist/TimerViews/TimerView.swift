4b68de636a1fcf83e7fcf8ea6ce79b691c35bc38
Marc Respass <marc@iliosinc.com>
2021-09-21 15:25:19 -0400

    Kinda big changes to the layout to fit larger text thinking of requiring iOS 15 in order to get text size limits

--- IronFist/TimerViews/TimerView.swift
+++ IronFist/TimerViews/TimerView.swift
@@ -7,28 +7,109 @@
 import SwiftUI
 import IronFistKit
 
+extension ContentSizeCategory {
+    func bestFont() -> Font {
+        switch self {
+            case .extraSmall, .small, .medium, .large, .extraLarge:
+                return .title
+            case .extraExtraLarge, .extraExtraExtraLarge, .accessibilityMedium:
+                return .title2
+            case .accessibilityLarge:
+                return .title3
+            case .accessibilityExtraLarge:
+                return .subheadline
+            case .accessibilityExtraExtraLarge, .accessibilityExtraExtraExtraLarge:
+                return .caption
+            @unknown default:
+                return .body
+        }
+    }
+}
+
+// https://useyourloaf.com/blog/swiftui-custom-view-modifiers/
+// That’s a bit ugly. A better way is to add our custom modifier as an extension on View:
+struct RepeatCaption: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory: ContentSizeCategory
+
+    func body(content: Content) -> some View {
+        content
+            .font(sizeCategory.bestFont())
+            .padding([.leading, .trailing], 8)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.yellow)
+            )
+    }
+
+}
+
+struct RoundedButtonStyle: ButtonStyle {
+    var color: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding([.leading, .trailing], 8)
+            .background(self.color.cornerRadius(12))
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+    }
+}
+
+// https://www.fivestars.blog/articles/adaptive-swiftui-views/
 struct TimerView: View {
     @EnvironmentObject var controller: IronFistController
+    @Environment(\.sizeCategory) var sizeCategory: ContentSizeCategory
+
+    @ViewBuilder
+    func RepeatText(label: String) -> some View {
+        ButtonLabelText(label: label)
+            .padding(8)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 4)
+                    .foregroundColor(controller.circleState.timerCircleColor)
+            )
+    }
+
+    fileprivate func ButtonLabelText(label: String) -> Text {
+        return Text(label)
+            .fontWeight(sizeCategory > .extraExtraExtraLarge ? .regular : .bold)
+            .font(sizeCategory.bestFont())
+    }
 
     var body: some View {
         VStack {
-            HStack {
-                self.timerButton()
-                Spacer()
-                Text("Repeat: \(controller.repeatCount)/\(controller.maxRepetitions)")
-                    .fontWeight(.bold)
-                    .font(.title)
-                    .padding(8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(lineWidth: 4)
-                            .foregroundColor(controller.circleState.timerCircleColor)
-                    )
-
+            VStack {
+                Button {
+                    self.controller.toggleTimer()
+                } label: {
+                    ButtonLabelText(label: self.controller.timerRunning ? "Stop" : "Begin")
+                        .padding(8)
+                }
+                .buttonStyle(RoundedButtonStyle(color: self.controller.timerRunning ? .red : .green))
+//                .padding(.bottom)
+//
+//                RepeatText(label: "Repeat: \(controller.repeatCount)/\(controller.maxRepetitions)")
             }
-            .padding([.leading, .trailing])
 
             TimerProgressCircle()
+            HStack {
+                Text("Repeat: \(controller.repeatCount)X")
+                Spacer()
+                Text("\(controller.repeatCount) of \(controller.maxRepetitions)")
+            }
+            .font(.title3)
+            .overlay(Divider().background(Color.black), alignment: .bottom) // https://stackoverflow.com/a/64921380
+//            .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom)
+//                        .foregroundColor(Color.gray), alignment: .bottom)
+
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 12)
+//                    .stroke(lineWidth: 4)
+//                    .foregroundColor(controller.circleState.timerCircleColor)
+//            )
+
             if let ironFist = controller.selectedIronFist {
                 self.ironFistText(ironFist)
             } else {
@@ -48,15 +129,6 @@ struct TimerView: View {
 
 // MARK: - Private View methods
 extension TimerView {
-    fileprivate func timerButton() -> some View {
-        Button {
-            self.controller.toggleTimer()
-        } label: {
-            self.controller.stopBeginButtonLabel()
-        }
-        .buttonStyle(RoundedButtonStyle(color: self.controller.timerRunning ? .red : .green))
-    }
-
     fileprivate func ironFistText(_ ironFist: IronFist) -> some View {
         VStack(alignment: .leading) {
             HStack {
@@ -67,8 +139,10 @@ extension TimerView {
             .font(.title.weight(.semibold))
             Text(ironFist.instruction)
                 .font(.body.weight(.medium))
-            Text(ironFist.motivation)
-                .font(.body.italic())
+            if sizeCategory < .extraExtraExtraLarge {
+                Text(ironFist.motivation)
+                    .font(.body.italic())
+            }
         }
         .padding()
         .overlay(
@@ -93,13 +167,27 @@ extension TimerView {
 }
 
 #if DEBUG
+// extraSmall
+// small
+// medium
+// large
+// extraLarge
+// extraExtraLarge
+// extraExtraExtraLarge
+// accessibilityMedium
+// accessibilityLarge
+// accessibilityExtraLarge
+// accessibilityExtraExtraLarge
+// accessibilityExtraExtraExtraLarge
+
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             TimerView()
-                .preferredColorScheme(.dark)
-                .environmentObject(IronFistController())
-                .environment(\.locale, .init(identifier: "es"))
+//                .preferredColorScheme(.dark)
+                .environmentObject(IronFistController.readyController)
+                .environment(\.sizeCategory, .large) // normal is .large
+                .environment(\.locale, .init(identifier: "en"))
         }
     }
 }
